52a456eeef2abea530392fce7f7da7ec0cb39b68
JHOSEP32 <jhon9796@hotmail.com>
2017-02-03 02:20:03 -0500

    Creados los modelos y las migraciones de la seccion de ventas

---
+++ database/migrations/2017_02_03_020435_create_sales_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSalesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->integer('user')->unsigned();
+            $table->integer('client')->unsigned();
+            $table->dateTime('datetime');
+            $table->double('total');
+            $table->enum('state', ['En proceso', 'Finalizada']);
+            $table->foreign('user')->references('id')->on('users');
+            $table->foreign('client')->references('id')->on('clients');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sales');
+    }
+}
